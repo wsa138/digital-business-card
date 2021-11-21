1535f8758e1979be1075e29531a7cc837fad121f
William Addesso <wsaddesso@gmail.com>
2021-11-21 01:43:33 -0500

    Added elements to CardHead.

--- src/CardHead.js
+++ src/CardHead.js
@@ -10,6 +10,11 @@ class CardHead extends Component {
     return (
       <div>
         <img src={me} alt="Photo of me" />
+        <h1>Billy Addesso</h1>
+        <h2>Frontend Developer</h2>
+        <p>wsa138.github.io/bootstrap-portfolio</p>
+        <button>Email</button>
+        <button>Linkedin</button>
       </div>
     );
   }
